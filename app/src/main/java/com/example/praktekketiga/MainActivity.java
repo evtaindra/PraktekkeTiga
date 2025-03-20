eb12be8cd94f88cac57fe396f21d7eb7e52bf6b9
evtaindra <evtapribadi@gmail.com>
2025-03-20 10:07:51 +0700

    Mengimplementasikan layout dan fungsionalitas pada MainActivity: - Menambahkan background `bg_main.png` pada `activity_main.xml`. - Menambahkan `TextView` (txtmainAkun) yang menampilkan ucapan selamat datang dan username. - Menambahkan `ImageView` (mhs.png) untuk ikon. - Menambahkan `Button` (btMainExit) dengan label "Keluar" yang digunakan untuk keluar dari aplikasi. - Mengatur warna background button `btMainExit` menjadi kuning. - Mengatur logic keluar dari aplikasi dengan menampilkan `AlertDialog` konfirmasi terlebih dahulu. - Mengirim data username dari `LoginActivity` ke `MainActivity`. - Mengubah warna button `Batal` di `activity_login.xml` menjadi kuning. - Menambahkan gambar background `bg_main.png` dan gambar icon `mhs.png`.

--- app/src/main/java/com/example/praktekketiga/MainActivity.java
+++ app/src/main/java/com/example/praktekketiga/MainActivity.java
@@ -1,24 +1,45 @@
 package com.example.praktekketiga;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView txtmainAkun;
+Button btMainKeluar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        txtmainAkun = findViewById(R.id.txtmainAkun);
+        btMainKeluar = findViewById(R.id.btMainExit);
+        String username = getIntent().getStringExtra("USERNAME");
+        txtmainAkun.setText(txtmainAkun.getText().toString() + username);
+        btMainKeluar.setOnClickListener(v ->{
+            Intent i = new Intent(MainActivity.this, LoginActivity.class);
+            new AlertDialog.Builder(this)
+                    .setTitle("Konfirmasi")
+                    .setMessage("Apakah Anda yakin ingin keluar?")
+                    .setNegativeButton("Tidak", null)
+                    .setPositiveButton("Ya", (dialog, which) ->{
+                            // Hapus data username dari Intent
+                            i.removeExtra("USERNAME");
+                            // Tambahkan kode untuk keluar dari aplikasi di sini
+                            startActivity(i);
+                            finish(); // Tutup MainActivity setelah pindah
+                })
+                .show();
         });
+
+
     }
 }
